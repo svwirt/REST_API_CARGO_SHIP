256b6bd92ae67b69d352db36f7efe355d333e8fa
svwirt <35933034+svwirt@users.noreply.github.com>
2018-11-11 01:21:25 -0700

    Update ship.py

--- resources/ship.py
+++ resources/ship.py
@@ -2,8 +2,7 @@
 from flask_restful import Resource, reqparse
 from models.ship import ShipModel
 
-
-class Ship(Resource):
+class ShipPost(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('type',
                         type=str,
@@ -20,25 +19,56 @@ class Ship(Resource):
                         required=True,
                         help="This field cannot be left blank!"
                         )
-    def get(self, name):
-        ship = ShipModel.find_by_name(name)
-        if ship:
-            return ship.json()
-        return {'message': 'Ship not found'}, 404
+    parser.add_argument('name',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
 
-    def post(self, name):
+    def post(self):
+        data = Ship.parser.parse_args()
+        name = data['name']
         if ShipModel.find_by_name(name):
             return {'message': "A ship with name '{}' already exists.".format(name)}, 400
 
         data = Ship.parser.parse_args()
 
-        ship = ShipModel(name, **data)
+        ship = ShipModel(name, data['type'], data['length'], data['shipSelf'])
         try:
             ship.save_to_db()
         except:
             return {"message": "An error occurred creating the ship."}, 500
 
         return ship.json(), 201
+
+class Ship(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('name',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('type',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('length',
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('shipSelf',
+                        type=str,
+                        # required=True,
+                        help="This field cannot be left blank!"
+                        )
+    def get(self, name):
+        ship = ShipModel.find_by_name(name)
+        if ship:
+            return ship.json()
+        return {'message': 'Ship not found'}, 404
+
 
     def delete(self, name):
         ship = ShipModel.find_by_name(name)
@@ -55,7 +85,7 @@ class Ship(Resource):
         if ship:
             ship.type = data['type']
             ship.length = data['length']
-            ship.shipSelf = data['shipSelf']
+            ship.shipSelf = "data['shipSelf']"
         else:
             ship = ShipModel(name, **data)
 
